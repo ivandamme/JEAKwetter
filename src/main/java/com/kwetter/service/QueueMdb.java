57adec861f58a78dd9221ce655f67dd7b0cdcab3
niek <niek_1993@hotmail.com>
2017-05-07 19:35:38 +0200

    JMS + Websocket working

--- src/main/java/com/kwetter/service/QueueMdb.java
+++ src/main/java/com/kwetter/service/QueueMdb.java
@@ -1,59 +1,63 @@
-//package com.kwetter.service;
-//
-//import javax.annotation.Resource;
-//import javax.ejb.ActivationConfigProperty;
-//import javax.ejb.MessageDriven;
-//import javax.jms.*;
-//
-///**
-// * Created by Niek on 6-5-2017.
-// */
-//
-//@MessageDriven(
-//        activationConfig = {
-//                @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
-//                @ActivationConfigProperty(propertyName = "destination", propertyValue = "testqueue")},
-//        mappedName = "testqueue")
-//public class QueueMdb {
-//
-//    @Resource(lookup = "jms/__defaultConnectionFactory")
-//    ConnectionFactory connectionFactory;
-//
-//    @Resource(lookup = "testtopic")
-//    private Topic topic;
-//
-//    /**
-//     * @see MessageListener#onMessage(Message)
-//     */
-//    public void onMessage(Message message) {
-//
-//        try {
-//            message.acknowledge();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        TextMessage txtMessage = (TextMessage) message;
-//
-//        try {
-//            System.out.println(txtMessage.getText());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        try {
-//            Connection connection = connectionFactory.createConnection();
-//            Session session = connection.createSession();
-//            MessageProducer producer = session.createProducer(topic);
-//            TextMessage textMessage = session.createTextMessage();
-//            textMessage.setText("Topic messagesage broadcasted from the MDB inside the Kwetter Server.");
-//            connection.start();
-//            producer.send(textMessage);
-//            System.out.println("Topic message broadcasted.");
-//            connection.close();
-//        } catch (JMSException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//}
+package com.kwetter.service;
+
+import com.kwetter.model.Kweet;
+import com.kwetter.model.User;
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.jms.*;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by Niek on 6-5-2017.
+ */
+
+@Named
+@MessageDriven(mappedName = "jms/KwetterGo", activationConfig = {@ActivationConfigProperty(propertyName = "acknowledgeMode",
+        propertyValue = "Auto-acknowledge"), @ActivationConfigProperty(propertyName = "destinationType",
+        propertyValue = "javax.jms.Queue")})
+public class QueueMdb implements Serializable, MessageListener{
+
+    private static final Logger LOG = Logger.getLogger(QueueMdb.class.getName());
+
+    @Resource
+    private MessageDrivenContext mdc;
+
+    @Inject
+    KweetService kweetService;
+
+    public QueueMdb() {
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            TextMessage txtMessage = (TextMessage) message;
+            String newMessage = txtMessage.getText();
+            LOG.info(newMessage);
+            String username = newMessage.split(" : ")[0];
+            String kweetText = newMessage.split(" : ")[1];
+
+            User user = kweetService.findByUserName(username);
+            if (user == null) {
+                System.out.println("User " + username + " doesn't seem to exist.");
+                return;
+            }
+            Kweet k = new Kweet(kweetText,user);
+            kweetService.placeKweet(k);
+
+            System.out.println("User " + username + " has created an Kweet.");
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Error in message: " + ex.getLocalizedMessage());
+            ex.printStackTrace();
+        }
+    }
+
+
+}
